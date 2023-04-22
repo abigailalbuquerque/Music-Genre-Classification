7c03960246ab0e400f7c678744a3e615d57a515f
Ryan Darcey <rtdarcey@wpi.edu>
2023-04-22 13:57:25 -0400

    a

--- feature_extraction.py
+++ feature_extraction.py
@@ -14,36 +14,36 @@ for genre in genres:
         # sr is sampling rate, which is none in this case because we want the whole sample to be used
         x, sr = librosa.load('wavfiles/' + genre + '/' + song, sr=None)
 
-            # Extracting features and putting them into the csv
-            # Zero Crossings
-            n0 = 9000
-            n1 = 9100
-            zero_crossings = librosa.zero_crossings(x[n0:n1], pad=False)
-            print(sum(zero_crossings))
+        # Extracting features and putting them into the csv
+        # Zero Crossings
+        n0 = 9000
+        n1 = 9100
+        zero_crossings = librosa.zero_crossings(x[n0:n1], pad=False)
+        print(sum(zero_crossings))
 
-            # Spectral Centroid
-            spectral_centroids = librosa.feature.spectral_centroid(y=x, sr=sr)
-            print(np.mean(spectral_centroids))
+        # Spectral Centroid
+        spectral_centroids = librosa.feature.spectral_centroid(y=x, sr=sr)
+        print(np.mean(spectral_centroids))
 
-            # Spectral Roll-off
-            spectral_rolloff = librosa.feature.spectral_rolloff(y=x, sr=sr)
-            print(np.mean(spectral_rolloff))
+        # Spectral Roll-off
+        spectral_rolloff = librosa.feature.spectral_rolloff(y=x, sr=sr)
+        print(np.mean(spectral_rolloff))
 
-            # Chroma Filters
-            chroma_stft = librosa.feature.chroma_stft(y=x, sr=sr)
-            print(np.mean(chroma_stft))
+        # Chroma Filters
+        chroma_stft = librosa.feature.chroma_stft(y=x, sr=sr)
+        print(np.mean(chroma_stft))
 
-            # Root Mean Squared Energy
-            rms = librosa.feature.rms(y=x)
-            print(np.mean(rms))
+        # Root Mean Squared Energy
+        rms = librosa.feature.rms(y=x)
+        print(np.mean(rms))
 
-            # Spectral Bandwidth
-            spec_bw = librosa.feature.spectral_bandwidth(y=x, sr=sr)
-            print(np.mean(spec_bw))
+        # Spectral Bandwidth
+        spec_bw = librosa.feature.spectral_bandwidth(y=x, sr=sr)
+        print(np.mean(spec_bw))
 
-            # mel-frequency cepstral coefficients
-            mfccs = librosa.feature.mfcc(y=x, sr=sr)
-            print(mfccs)
+        # mel-frequency cepstral coefficients
+        mfccs = librosa.feature.mfcc(y=x, sr=sr)
+        print(mfccs)
 
         to_append = f'{genre} {np.mean(chroma_stft)} {np.mean(rms)} {np.mean(spectral_centroids)} {np.mean(spec_bw)} {np.mean(spectral_rolloff)} {sum(zero_crossings)} '
         for e in mfccs:
