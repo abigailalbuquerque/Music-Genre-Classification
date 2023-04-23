56c01bd561f001da2f93835ffa0c110c97f9792d
AirhornRemix <48485921+AirhornRemix@users.noreply.github.com>
2023-04-22 20:44:18 -0400

    Add files via upload

---
+++ Vocal_Segregation_Loader.py
@@ -0,0 +1,50 @@
+import sys
+
+import numpy as np
+import matplotlib.pyplot as plt
+from IPython.display import Audio
+import soundfile as sf
+import os
+
+import librosa
+
+def seperator (song, genre):
+    # Loading test file
+    y, sr = librosa.load(genre+"/"+song)
+
+    #grab a sample of 20 seconds
+    S_full, phase = librosa.magphase(librosa.stft(y))
+    Audio(data=y[0 * sr:20 * sr], rate=sr)
+
+    # Build filters based on cosine similarity, hopefully we get vocalish frequencies together
+    S_filter = librosa.decompose.nn_filter(S_full, aggregate=np.median, metric='cosine', width=int(librosa.time_to_frames(2, sr=sr)))
+    S_filter = np.minimum(S_full, S_filter)
+
+    # Margins to reduce crossover
+    margin_i, margin_v = 2, 10
+    power = 2
+    #edit above values for tweaking
+    mask_i = librosa.util.softmask(S_filter, margin_i * (S_full - S_filter), power=power)
+    mask_v = librosa.util.softmask(S_full - S_filter, margin_v * S_filter, power=power)
+    S_foreground = mask_v * S_full
+    S_background = mask_i * S_full
+
+    #Save and export foreground audio
+    y_foreground = librosa.istft(S_foreground * phase)
+    Audio(data=y_foreground[0 * sr:20 * sr], rate=sr)
+    sf.write(genre+'_separated/vocal_file_'+song+'.wav', y_foreground[0 * sr:20 * sr], sr, 'PCM_24')
+
+    # Save and export background audio
+    y_background = librosa.istft(S_background * phase)
+    Audio(data=y_background[0 * sr:20 * sr], rate=sr)
+    sf.write(genre+'_separated/instrument_file_'+song+'.wav', y_background[0 * sr:20 * sr], sr, 'PCM_24')
+
+
+if __name__ == "__main__":
+    genreList = ["Blues", "Classical", "Country", "Electronic", "Hip Hop", "Jazz", "Metal", "Pop", "Reggae", "Rock"]
+    for i in range(len(genreList)):
+        for filename in os.listdir(genreList[i]):
+            f = os.path.join(genreList[i], filename)
+            if os.path.isfile(f):
+                seperator(filename, genreList[i])
+    sys.exit(0)
